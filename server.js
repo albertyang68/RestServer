1b8784113a40369f80a64b4d5a6c15d1aeb88651
yzq9652 <z274yang@gmail.com>
2016-06-01 17:23:56 -0400

    add elk to test
    see the request body

--- server.js
+++ server.js
@@ -5,18 +5,45 @@ var googleApis = require('googleapis');
 var request = require('request');
 var app = express();
 var analyticsreporting = googleApis.analyticsreporting("v4");
+var elasticsearch = require('elasticsearch');
+var http_aws_es = require('http-aws-es');
+var AWS = require('aws-sdk');
 
 app.post("/", function(req, res) {
   var type = req.get("Type").toLowerCase().replace(/ /g, '');
   if(type == 'googleanalytics') {
-
+    config.googleanalytics = {}
     config.googleanalytics.client_email = req.get("clientemail").trim();
     config.googleanalytics.private_key = req.get("privatekey").replace(/\\n/g, "\n");
+    config.googleanalytics.reportRequests = [{}];
     config.googleanalytics.reportRequests[0].viewId = req.get("viewid").trim();
-    fs.writeFileSync('./config.json', JSON.stringify(config));
     console.log(config.googleanalytics.client_email);
+    fs.writeFileSync('./config.json', JSON.stringify(config));
     Google(res)
-  }
+  } else if(type == 'elk') {
+    config.elasticSearch = {}
+    config.elasticSearch.url = req.get("url").trim();
+    config.elasticSearch.log = req.get("log").trim();
+    config.elasticSearch.region = req.get("region").trim();
+    config.elasticSearch.use_aws_auth = req.get("auth").trim();
+    config.elasticSearch.aws_credentials = {};
+    config.elasticSearch.aws_credentials.access = req.get("awsaccess").trim();
+    config.elasticSearch.aws_credentials.secret = req.get("awssecret").trim();
+    config.elasticSearch.query = JSON.parse(req.get("query"));
+    fs.writeFileSync('./config.json', JSON.stringify(config));
+    es(res)
+  } else if(type == 'aws') {
+    config.aws = {}
+    config.aws.region = req.get("region").trim();
+    config.aws.time_ago = req.get("timeage").trim();
+    config.aws.metrics = []
+    config.params = {};
+    config.params.nameSpace = req.get("namespace").trim();
+    config.params.period = 60;
+    config.params.statistics = ["Minimum", "Maximum", "Average", "SampleCount", "Sum"],
+    config.params.dimensions = [];
+    console.log(req.body);
+    res.send(req.body)
 })
 
 var Google = function(res) {
@@ -39,5 +66,37 @@ var Google = function(res) {
       res.send(response)
     })
 }
+
+var es = function(res){
+  config = config.elasticSearch;
+  if (config.use_aws_auth == 'true') {
+    this.client = new elasticsearch.Client({
+      host: config.url,
+      log:  config.log,
+      connectionClass: require('http-aws-es'),
+      amazonES: {
+          region: config.region,
+          accessKey: config.aws_credentials.access,
+          secretKey: config.aws_credentials.secret,
+      }
+    });
+  } else {
+    this.client = new elasticsearch.Client({
+      host: config.url,
+      log:  config.log
+    });
+  }
+
+  this.client.search(
+      config.query,
+      function(err, response){
+        if (err)
+          return res.send(err);
+        var hits = response.hits.hits;
+        res.send(hits);
+      }
+  );
+};
+
 
 app.listen(3000)
